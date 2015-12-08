982c4599596b3655d9655e22f1b254f669a4d6fc
Bernhard Grünewaldt <github@gruenewaldt.net>
2015-12-08 19:06:29 +0100

    v1.1 api

---
+++ 1.1/api.js
@@ -0,0 +1,24 @@
+var url = "./swagger.json";
+window.swaggerUi = new SwaggerUi({
+    url: url,
+    dom_id: "swagger-ui-container",
+    supportedSubmitMethods: [ ],
+    onComplete: function(swaggerApi, swaggerUi){ },
+    onFailure: function(data) { alert(data); },
+    docExpansion: "list",
+    operationsSorter: function (a, b) {
+        var aNormalized = a.path;
+        var bNormalized = b.path;
+
+        /** NORMAL COMPARISON */
+        if (aNormalized.length > bNormalized.length) {
+            return 1;
+        }
+        if (aNormalized.length < bNormalized.length) {
+            return -1;
+        }
+        return 0;
+    },
+    withCredentials: false
+});
+window.swaggerUi.load();
